5423b38b99c749f0c7ce681ee03b20cb0d4587b5
Kirill <Anotherzerosun@gmail.com>
2024-11-19 00:55:07 +0300

    api ready to use

--- web/alg.py
+++ web/alg.py
@@ -1,9 +1,7 @@
 import numpy as np
 
 numbers64 = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_'
-#z_dim = 100
-#batch_size = 32
-#noise = np.random.normal(0, 1, size=(batch_size, z_dim))
+
 
 
 def convert_10_into_64(x: int) -> str:
@@ -11,7 +9,7 @@ def convert_10_into_64(x: int) -> str:
     while x:
         y += numbers64[x % 64]
         x //= 64
-        return y
+    return y
 
 
 def convert_64_into_10(x: str) -> int:
@@ -26,7 +24,8 @@ def convert_vectors_10_into_64cod(vv: list) -> str:  # шифрование ве
     for v in vv:
         if v > 0:
             vi = str(int(v//(0.1**16)))
-            vi = '2' + '0' * (17 - len(vi)) + vi  # сохраняем знак + ввиде цифры 2 перед числом        else:
+            vi = '2' + '0' * (17 - len(vi)) + vi  # сохраняем знак + ввиде цифры 2 перед числом
+        else:
             vi = str(int(abs(v)//(0.1**16)))
             vi = '1' + '0' * (17 - len(vi)) + vi  # сохраняем знак - ввиде цифры 1 перед числом
         sl += vi  # получаем строку-число унифицированной формы вида скейка(чётность{2/1} + число без точки)
@@ -41,6 +40,15 @@ def convert_64cod_into_vectors_10(s: str) -> list:  # дешифрование 6
     for j in range(100):
         prv = k[18*j: 18*(1+j)]  # берём по 18 символов отведённые на каждое значение
         prv = sgn[prv[0]] * (np.float64(prv[1:]) * (0.1**16))  # вытаскиваем знак
-    #print(prv)
-    v.append(prv)
+        #print(prv)
+        v.append(prv)
     return v
+
+
+if __name__ == '__main__':
+    z_dim = 100
+    batch_size = 1
+    noise = np.random.normal(0, 1, size=(batch_size, z_dim))
+    print(noise[0])
+    print(convert_64cod_into_vectors_10(convert_vectors_10_into_64cod(noise[0])))
+    print(convert_64_into_10(convert_10_into_64(124)))
--- web/api.py
+++ web/api.py
@@ -1,10 +1,28 @@
 import flask
-from alg import convert_64cod_into_vectors_10
+from alg import *
 import os
-#from work import generate_by_vector
+import matplotlib.pyplot as plt
+import numpy as np
+from keras.models import load_model
 
 app = flask.Flask(__name__)
 app.config['SECRET_KEY'] = 'kkkk'
+
+
+def generate_by_vector(v, gen_path=f'../neural network/wsaves/generator_epoch_{2500}.h5'):
+        #  конвертим под подходящий тип вектор
+    converted_v0 = np.array(v)
+    converted_v = np.array(np.ndarray(shape=(100,), buffer=converted_v0), ndmin=2)
+        #  создаём кратинку
+    gen = load_model(gen_path, compile=False)  # Берём нужную модель
+    generated_image = gen.predict(converted_v)[0]
+    plt.figure(figsize=(10, 10))
+    plt.imshow((generated_image * 0.5) + 0.5)  # Восстановление из нормализации
+    plt.axis('off')  # почему через matlib?
+    plt.tight_layout()  # картинка возвращается в виде массива и проще всего её сделать там
+    plt.savefig('buffer/image.jpg')  # сохраняем загенеренную картинку
+    return True
+
 
 @app.route('/')
 def home():
@@ -12,15 +30,15 @@ def home():
 
 @app.route('/<string:code64>')
 def generate(code64):
-    """
-    try:
-        os.remove('buffer/image.jpg')
-    except Exception:
-        pass
-    v = convert_64cod_into_vectors_10(code64)
-    generate_by_vector(v, 'buffer/image.jpg')
-    """
-    return '<img src="buffer\image.jpg">'
+    if code64 and (code64 != 'favicon.ico'):
+        try:
+            os.remove('buffer/image.jpg')  # очищаем буфер от предыдущей картинки
+        except Exception:
+            pass
+        v = convert_64cod_into_vectors_10(code64)
+        generate_by_vector(v)  # создаём новую
+
+        return '<img src="buffer\image.png">'
 
 
 if __name__ == '__main__':
