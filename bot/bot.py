fa37a14961320ab99674f483d2b7d5447c12e56e
Kirill Pletyago <91794978+notn1cel@users.noreply.github.com>
2024-11-24 15:33:44 +0300

    Update bot.py
    Добавил /generate

--- bot/bot.py
+++ bot/bot.py
@@ -1,7 +1,10 @@
 import telebot
+import requests
 
 TOKEN = "7953465629:AAH7OOq7U9-f-KYu9A1_BDsONhi1UNoJeGY"
 bot = telebot.TeleBot(TOKEN)
+
+API_URL = "http://localhost:5000/generate" # Вставить адрес, где генерируется аниме-девочка, иначе -вайб
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
@@ -18,8 +21,21 @@ def send_info(message):
         "Кирилл Плетяго (Проектирование интерфейса для взаимодействия с нейросетью. Создание сайта и телеграм бота)."
     )
 
-# Добавить /generate
-
+@bot.message_handler(commands=['generate'])
+def generate_image(message):
+    try:
+        response = requests.get(API_URL)
+        
+        if response.status_code == 200:
+            with open("generated_image.jpg", "wb") as file:
+                file.write(response.content)
+            
+            with open("generated_image.jpg", "rb") as photo:
+                bot.send_photo(message.chat.id, photo)
+        else:
+            bot.reply_to(message, f"Ошибка генерации изображения: {response.status_code}")
+    except Exception as e:
+        bot.reply_to(message, f"Произошла ошибка: {e}")
 
 if __name__ == '__main__':
     print("Бот запущен...")
